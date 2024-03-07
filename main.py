c4ce00e8129580b689db454a612b42558fa7644e
JustLon <lgabrielmonzon@hotmail.com>
2024-03-06 20:44:00 -0600

    Reformateados los comentarios y pruebas

--- main.py
+++ main.py
@@ -27,36 +27,37 @@ class FaceRecognitionApp:
         self.video_label = tk.Label(self.root)
         self.video_label.pack(padx=10, pady=10)
 
-        # Load reference images from the "caras" directory
+        # Se cargan las caras (La carpeta) para tener las referencias
         self.reference_encodings = self.load_reference_encodings("caras")
 
-        # Load user data from JSON file
+        #Se carga el Json con los datos de los usuarios
         self.user_data = self.load_user_data("datos/datos.json")
 
         self.fps = fps
 
-        # Using threading for showing frame
+        # Se usa un hilo separado para mostrar el marco (frame)
         self.thread = threading.Thread(target=self.show_frame)
         self.thread.daemon = True
         self.thread.start()
 
-        # Add buttons at the bottom
+        # Botones de agregar y salir
         self.add_person_button = tk.Button(self.root, text="Agregar Persona", command=self.add_person)
         self.add_person_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-        self.start_stop_script_button = tk.Button(self.root, text="Parar Programa", command=self.stop)
+        self.start_stop_script_button = tk.Button(self.root, text="Salir", command=self.stop)
         self.start_stop_script_button.pack(side=tk.RIGHT, padx=10, pady=10)
 
         self.file_path = None  # Variable para almacenar la ruta del archivo seleccionado
 
     def load_user_data(self, json_file):
+        #json para los datos del usuario
         if exists(json_file):
             with open(json_file, "r") as file:
                 return json.load(file)
         else:
             return []
 
-    def load_reference_encodings(self, directory):
+    def load_reference_encodings(self, directory): #se cargan las referencias
         reference_encodings = []
         onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
         for filename in onlyfiles:
@@ -77,8 +78,8 @@ class FaceRecognitionApp:
             if not ret:
                 continue
 
-            # Capture frames at a lower resolution
-            frame = cv2.resize(frame, (640, 480))  # Adjust resolution as needed
+            # frame a baja res
+            frame = cv2.resize(frame, (640, 480))  # Se ajusta el frame final
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = self.adjust_color_balance(frame)
@@ -90,9 +91,9 @@ class FaceRecognitionApp:
 
                 for face_location, face_encoding in zip(face_locations, face_encodings):
                     label = "Desconocido"
-                    color = (0, 0, 255)  # Red color for unknown face
+                    color = (0, 0, 255)  # Rojo para persona que no se reconoce
 
-                    # Compare face encoding with all reference encodings
+                    # Se comparan las caras con las ref.
                     for i, reference_encoding in enumerate(self.reference_encodings):
                         results = face_recognition.compare_faces([reference_encoding], face_encoding)
                         if results[0]:
@@ -103,9 +104,10 @@ class FaceRecognitionApp:
                                 if user["id"] == user_id:
                                     label = f"{user['nombre']}, {user['edad']}"
                                     break
-                            color = (0, 255, 0)  # Green color for recognized face
-                            break  # Exit loop if face is recognized
+                            color = (0, 255, 0)  # Verde si se reconoce la cara
+                            break  # Se reconoce entonces se sale del loop.
 
+                    #Se ponen los datos en la pantalla, cuadro y datos.
                     cv2.rectangle(frame, (face_location[3], face_location[0]), (face_location[1], face_location[2]), color, 2)
                     cv2.putText(frame, label, (face_location[3], face_location[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
 
@@ -118,67 +120,70 @@ class FaceRecognitionApp:
             self.video_label.update()
 
     def adjust_color_balance(self, frame):
-        # Adjust color balance if necessary
-        # frame[:, :, 0] = frame[:, :, 0] * 0.8
+        # Ajustes de color que se deban hacer.
         return frame
 
     def add_person(self):
-        # Function to add a new person
+        # Para Agregar a una persona nueva
         self.ventana_foto = tk.Toplevel(self.root)
         self.ventana_foto.title("Agregar persona")
         self.ventana_foto.geometry("400x200")
         self.ventana_foto.focus_force()
 
-        # Add a title
+        # Titulo de la ventana
         title_label = tk.Label(self.ventana_foto, text="Agregar persona", font=("Helvetica", 16, "bold"))
         title_label.pack(pady=10)
 
-        # Create a button for uploading a photo
+        # Boton para ingresar la imagen
         upload_button = tk.Button(self.ventana_foto, text="Subir foto", command=self.upload_photo)
         upload_button.pack(pady=10)
 
-        self.ventana_foto.focus_force()
+        self.ventana_foto.focus_force() #Mantener el foco de la ventana secundaria!
 
     def upload_photo(self):
-        # Function to handle uploading a photo
+        # Con esta funcion operamos la accion del boton de subir la foto
         print("Uploading photo...")
 
-        # Open a file explorer window to select an image file
+        # Se abre un explorador de archivos
         self.file_path = fd.askopenfilename(title="Seleccione una imagen", filetypes=(("Image files", "*.jpg;*.jpeg;*.png;*.bmp"), ("all files", "*.*")))
         if self.file_path:
             print("File selected:", self.file_path)
 
-            # Open and resize the image
+            # Se abre y se redimenciona la imagen
             image = Image.open(self.file_path)
-            image = image.resize((300, 300), Image.Resampling.LANCZOS)  # Resize the image to fit within a 300x300 area
+            image = image.resize((300, 300), Image.Resampling.LANCZOS)  # 300x300 para que quepa en la ventana
 
-            # Convert the image to a format that tkinter can display
+            # Se convierte a formato ImageTk.photoimage para que se pueda mostrar
             photo = ImageTk.PhotoImage(image)
 
-            # If there is an existing image label, destroy it
+            # Si ya hay una imagen entonces se elimina todo lo relacionado
             if hasattr(self, 'image_label'):
                 self.image_label.destroy()
+                self.name_label.destroy()
+                self.name_entry.destroy()
+                self.age_label.destroy()
+                self.age_entry.destroy()
 
-            # Create a Label to display the new image
+            # Se cre el display para la imagen
             self.image_label = tk.Label(self.ventana_foto, image=photo)
-            self.image_label.image = photo  # Keep a reference to the image to prevent it from being garbage collected
+            self.image_label.image = photo  # Se deja la imagen para prevenir que se vaya al mas alla
             self.image_label.pack(side="top", pady=10)
 
-            # Create entry widgets for name and age
-            name_label = tk.Label(self.ventana_foto, text="Nombre:")
-            name_label.pack()
+            # Widgets para el nombre y coso
+            self.name_label = tk.Label(self.ventana_foto, text="Nombre:")
+            self.name_label.pack()
             self.name_entry = tk.Entry(self.ventana_foto)
             self.name_entry.pack()
 
-            age_label = tk.Label(self.ventana_foto, text="Edad:")
-            age_label.pack()
+            self.age_label = tk.Label(self.ventana_foto, text="Edad:")
+            self.age_label.pack()
             self.age_entry = tk.Entry(self.ventana_foto)
             self.age_entry.pack()
 
             submit_button = tk.Button(self.ventana_foto, text="Aceptar", command=self.submit_photo)
             submit_button.pack()
 
-            self.ventana_foto.geometry("400x600")
+            self.ventana_foto.geometry("400x600") #Redimensionamos la ventana para que todo quepa
 
             self.ventana_foto.focus_set()
         else:
@@ -190,7 +195,7 @@ class FaceRecognitionApp:
         age = self.age_entry.get()
 
         if not name or not age:
-            print('No se lleno el campo nombre o edad')
+            print('No se lleno el campo nombre o edad')#Advertencia de windows
             messagebox.showwarning("Campos vacíos", "Por favor, completa todos los campos.")
             self.ventana_foto.focus_get()
             return
@@ -202,7 +207,7 @@ class FaceRecognitionApp:
             self.ventana_foto.focus_get()
             return
 
-        # Generar un ID único para el usuario
+        # Generar un ID unico para el usuario
         user_id = str(uuid.uuid4())
 
         # Se busca una cara en la imagen y se recorta
@@ -215,7 +220,7 @@ class FaceRecognitionApp:
             "edad": age
         }
 
-        # Guardar los datos del usuario en el archivo JSON
+        # Guardar los datos del usuario en el archivo json
         json_file = "datos/datos.json"
         if not exists(json_file):
             with open(json_file, "w") as file:
@@ -227,13 +232,13 @@ class FaceRecognitionApp:
                 file.seek(0)
                 json.dump(data, file, indent=4)
 
-        # Mover la imagen del usuario al directorio "caras" con el ID como nombre de archivo
+        # Mover la imagen del usuario al directorio de caras con el ID como nombre de archivo
         image_filename = user_id + "ori.jpg"
         image_destination = join("carasori", image_filename)
         shutil.copy(self.file_path, image_destination)
 
         print("Imagen Subida")
-        self.ventana_foto.destroy()
+        self.ventana_foto.destroy() # Salimos de la Imagen
 
         self.reference_encodings = self.load_reference_encodings("caras")
         self.user_data = self.load_user_data("datos/datos.json")
@@ -255,10 +260,10 @@ class FaceRecognitionApp:
                 # Obtener las coordenadas de la cara actual
                 top, right, bottom, left = face_location
 
-                # Recortar el área de la cara de la imagen
+                # Recortar el area de la cara de la imagen
                 cara_recortada = image[top:bottom, left:right]
 
-                # Guardar la cara recortada en la carpeta "caras" con el ID como nombre de archivo
+                # Guardar la cara recortada en la carpeta caras con el ID como nombre de archivo
                 if not exists("caras"):
                     os.makedirs("caras")
                 
